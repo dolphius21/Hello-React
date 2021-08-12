fda807c3efd2bfd0e422867636038688ad69011d
dolphius21 <johnrudolphlee@gmail.com>
2021-08-12 15:28:41 +0800

    done with sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,50 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaBirthdayCake, FaReact } from 'react-icons/fa';
+import {
+  FaReact,
+  FaBars,
+  FaBirthdayCake,
+  FaMapMarkerAlt
+} from 'react-icons/fa';
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const handleToggleClick = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
-    <div className="sidebar-container">
-      <div className="sidebar-header">
-        <FaReact className="logo" />
-        <h1 className="logo-text">Hello React</h1>
+    <>
+      <div className={`sidebar-container ${showSidebar && 'show-sidebar'}`}>
+        <div className="sidebar-header">
+          <FaReact className="logo" />
+          <h1 className="logo-text">Hello React</h1>
+        </div>
+        <nav className="nav-container">
+          <ul className="nav-links">
+            <li className="nav-link">
+              <NavLink to="/" activeClassName="active">
+                <FaBirthdayCake className="nav-icon" />
+                <p>Birthdays</p>
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink to="/birthdays" activeClassName="active">
+                <FaMapMarkerAlt className="nav-icon" />
+                <p>Tours</p>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className="nav-container">
-        <ul className="nav-links">
-          <li className="nav-link">
-            <NavLink to="/" activeClassName="active">
-              <FaBirthdayCake className="nav-icon" />
-              <p>Birthday Reminder</p>
-            </NavLink>
-          </li>
-          <li className="nav-link">
-            <NavLink to="/birthdays" activeClassName="active">
-              <FaBirthdayCake className="nav-icon" />
-              <p>Birthday Reminder</p>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+      <div
+        className={`toggle-btn ${showSidebar && 'active-sidebar'}`}
+        onClick={handleToggleClick}
+      >
+        <FaBars />
+      </div>
+    </>
   );
 };
 
